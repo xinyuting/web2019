0eb20198846ca564a6756eac8f54ccf9a5f17bea
xinyuting <648766127@qq.com>
2019-10-29 17:33:49 +0800

    测试1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int i=1;
+        if(i==1){
+            System.out.println("hahahah");
+        }
+    }
+}
