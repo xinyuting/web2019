d5514872ca833f1dea675d845045f581dea19b37
xinyuting <648766127@qq.com>
2019-10-29 17:44:20 +0800

    测试2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int i=1;
-        if(i==1){
-            System.out.println("hahahah");
-        }
+
     }
 }
